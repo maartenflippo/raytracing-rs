a1b331f651df784731987440d010fcccee53a2f1
Maarten Flippo <maartenflippo@outlook.com>
2022-04-09 13:11:10 +0200

    Finish chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 mod math;
+mod ray;
 
 use std::io::Write;
 
 use indicatif::ProgressBar;
+use math::Color;
+use ray::Ray;
+
+use crate::math::{Point3, Vec3};
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction().unit();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn write_color(w: &mut impl Write, color: &math::Color) {
     writeln!(
@@ -16,8 +28,19 @@ fn write_color(w: &mut impl Write, color: &math::Color) {
 }
 
 fn main() {
-    const WIDTH: usize = 256;
-    const HEIGHT: usize = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const WIDTH: usize = 400;
+    const HEIGHT: usize = (WIDTH as f64 / ASPECT_RATIO) as usize;
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", WIDTH, HEIGHT);
@@ -27,11 +50,13 @@ fn main() {
 
     for j in (0..HEIGHT).rev() {
         for i in 0..WIDTH {
-            let r = (i as f64) / (WIDTH - 1) as f64;
-            let g = (j as f64) / (HEIGHT - 1) as f64;
-            let b = 0.25;
+            let u = (i as f64) / (WIDTH - 1) as f64;
+            let v = (j as f64) / (HEIGHT - 1) as f64;
 
-            write_color(&mut std::io::stdout(), &math::Color::new(r, g, b));
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let color = ray_color(&ray);
+
+            write_color(&mut std::io::stdout(), &color);
             bar.inc(1);
         }
     }
