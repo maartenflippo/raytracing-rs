041113b9e8f3c96be494ce984b3bda8c84e71b5d
Maarten Flippo <maartenflippo@outlook.com>
2022-04-13 16:56:46 +0200

    Finish chapter 13

---
+++ README.md
@@ -0,0 +1,7 @@
+# Raytracer in Rust
+
+A raytracer in written in Rust.
+
+## Acknowledgements
+
+[_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
--- src/main.rs
+++ src/main.rs
@@ -59,51 +59,52 @@ fn write_color(w: &mut impl Write, color: &math::Color, samples_per_pixel: usize
 }
 
 fn main() {
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const WIDTH: usize = 400;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const WIDTH: usize = 800;
     const HEIGHT: usize = (WIDTH as f64 / ASPECT_RATIO) as usize;
-    const SAMPLES_PER_PIXEL: usize = 100;
+    const SAMPLES_PER_PIXEL: usize = 50;
     const MAX_DEPTH: usize = 50;
 
-    let mut world = HittableList::new();
-
     let mut rng = rand::thread_rng();
-    let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
-    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    // let mut world = HittableList::new();
 
-    world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        Rc::clone(&material_ground),
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.0),
-        0.5,
-        Rc::clone(&material_center),
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.5,
-        Rc::clone(&material_left),
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        -0.45,
-        Rc::clone(&material_left),
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.5,
-        Rc::clone(&material_right),
-    )));
+    // let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    // let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    // let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
+    // let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_at = Point3::new(0.0, 0.0, -1.0);
+    // world.add(Box::new(Sphere::new(
+    //     Vec3::new(0.0, -100.5, -1.0),
+    //     100.0,
+    //     Rc::clone(&material_ground),
+    // )));
+    // world.add(Box::new(Sphere::new(
+    //     Vec3::new(0.0, 0.0, -1.0),
+    //     0.5,
+    //     Rc::clone(&material_center),
+    // )));
+    // world.add(Box::new(Sphere::new(
+    //     Vec3::new(-1.0, 0.0, -1.0),
+    //     0.5,
+    //     Rc::clone(&material_left),
+    // )));
+    // world.add(Box::new(Sphere::new(
+    //     Vec3::new(-1.0, 0.0, -1.0),
+    //     -0.45,
+    //     Rc::clone(&material_left),
+    // )));
+    // world.add(Box::new(Sphere::new(
+    //     Vec3::new(1.0, 0.0, -1.0),
+    //     0.5,
+    //     Rc::clone(&material_right),
+    // )));
+    let world = random_scene(&mut rng);
+
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_at = Point3::new(0.0, 0.0, 0.0);
     let v_up = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (look_from - look_at).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let camera = Camera::new(
         look_from,
@@ -139,4 +140,74 @@ fn main() {
     }
 
     bar.finish();
+}
+
+fn random_scene(rng: &mut impl Rng) -> HittableList {
+    let mut world = HittableList::new();
+
+    let ground_material: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        Rc::clone(&ground_material),
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.gen_range(0.0..1.0);
+            let center = Point3::new(
+                a as f64 + 0.9 * rng.gen_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9 * rng.gen_range(0.0..1.0),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random(rng) * Color::random(rng);
+                    world.add(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Lambertian::new(albedo)),
+                    )));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_in_range(rng, 0.5, 1.0);
+                    let fuzz = rng.gen_range(0.0..0.5);
+                    world.add(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Metal::new(albedo, fuzz)),
+                    )));
+                } else {
+                    // glass
+                    world.add(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Dielectric::new(1.5)),
+                    )));
+                }
+            }
+        }
+    }
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Dielectric::new(1.5)),
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1))),
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)),
+    )));
+
+    world
 }
